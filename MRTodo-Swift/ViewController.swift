ab2c5eca49576147fab5b4aa1600d204ece8feb4
MrSouthWall <147359717+MrSouthWall@users.noreply.github.com>
2024-08-09 13:06:52 +0800

    Initial Commit

---
+++ MRTodo-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MRTodo-Swift
+//
+//  Created by 南墙先生 on 2024/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MRTodo-SwiftTests/MRTodo_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  MRTodo_SwiftTests.swift
+//  MRTodo-SwiftTests
+//
+//  Created by 南墙先生 on 2024/8/9.
+//
+
+import Testing
+@testable import MRTodo_Swift
+
+struct MRTodo_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
